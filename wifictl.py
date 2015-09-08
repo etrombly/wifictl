60ccc6004390624a734bb2cca7d60286f7e49dbc
etrombly <etrombly@yahoo.com>
2015-09-08 07:21:49 +0200

    parses .network and wpa config files

--- wifictl.py
+++ wifictl.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import subprocess
+import configparser
 import os
+import re
 
 class Interface():
     def __init__(self, name, IEEEtype = None, essid = None):
@@ -13,6 +15,9 @@ class Interface():
             self.essid = None
         else:
             self.essid = essid
+        self.networkConfig = configparser.ConfigParser()
+        self.networkConfig.optionxform=str
+        self.networks = []
 
     def __repr__(self):
         return self.name
@@ -34,64 +39,117 @@ class Interface():
             out = subprocess.check_output(["iwlist", self.name, "scan"])
             out = out.decode('utf-8')
             for cell in out.split("          Cell ")[1:]:
-                for line in cell.split("\n"):
-                    if "ESSID" in line:
-                        essid = line.split(":")[1].strip('"')
-                aps.append(AP(essid))
+                aps.append(AP(*self.parseCell(cell)))
         except subprocess.CalledProcessError:
             print("could not scan", self.name)
         return aps
-
+    
+    def parseCell(self, cell):
+        for line in cell.splitlines():
+            if "ESSID" in line:
+                essid = line.split(":")[1].strip('"')
+            elif "Protocol" in line:
+                protocol = line.split(":")[1].replace("IEEE ", "")
+            elif "Quality" in line:
+                pattern = re.compile('Quality=(\d{1,3})/(\d{1,3})\s*Signal level=(\d{1,3})/(\d{1,3})')
+                match = pattern.findall(line)
+                if match:
+                    quality = (int(match[0][0]) / int(match[0][1])) * 100
+                    signal = (int(match[0][2]) / int(match[0][3])) * 100
+        return essid, protocol, quality, signal
+                            
     def loadCFG(self):
         networkFile = "/etc/systemd/network/%s.network" % self.name
         wpaFile = "/etc/wpa_supplicant/wpa_supplicant-%s.conf" % self.name
-        if not os.path.exists(networkFile):
+        try:
+            self.networkConfig.readfp(open(networkFile))
+        except FileNotFoundError:
             print("network config for %s does not exist, creating" % (self.name))
-        if not os.path.exists(wpaFile):
+            self.writeNetworkCFG()
+        try:
+            with open(wpaFile) as wpaConfig:
+                networks = wpaConfig.read().split("network={\n")[1:]
+            self.networks = []
+            for network in networks:
+                self.networks.append(Network(*self.parseNetwork(network)))
+        except IOError:
             print("wpa_supplicant config for %s does not exist" % (self.name))
 
-    def createNetworkCFG(self):
-        with open("/etc/systemd/network/%s.network" % self.name, "r+") as networkFile:
+    def parseNetwork(self, network):
+        for line in network.splitlines():
+            line = line.lstrip()
+            if not line.startswith("#") and not line == "}":
+                if line.startswith("ssid="):
+                    ssid = line.split("ssid=")[1].strip('"')
+                if line.startswith("psk="):
+                    psk = line.split("psk=")[1].strip('"')
+        return ssid, psk
+
+    def writeNetworkCFG(self):
+        #test for now
+        self.networkConfig.add_section("Match")
+        self.networkConfig.set("Match", "Name", self.name)
+        self.networkConfig.add_section("Network")
+        self.networkConfig.set("Network", "DHCP", "ipv4")
+        self.networkConfig.write(open("/etc/systemd/network/%s.network" % self.name, 'w+'), space_around_delimiters=False)
+
+    def writeWPACFG(self):
+        with open("/etc/wpa_supplicant/wpa_supplicant-%s.conf" % self.name, 'w+') as wpaFile:
             pass
 
-    def createWPACFG(self):
-        with open("/etc/wpa_supplicant/wpa_supplicant-%s.conf" % self.name, "r+") as wpaFile:
-            pass
+    def findIF(self = None):
+        command = ["iwconfig"]
+        if self:
+            command.append(self.name)
+        out = subprocess.check_output(command, stderr = subprocess.DEVNULL)
+        out = out.decode('utf-8')
+        result = []
+        interfaces = out.split('\n\n')
+        interfaces = [x for x in interfaces if x]
+        for interface in interfaces:
+            for line in interface.splitlines():
+                if not line.startswith(" "):
+                    name = line.split()[0]
+                    try:
+                        IEEEtype = line.split("IEEE ")[1].split()[0]
+                    except IndexError:
+                        IEEEtype = None
+                    try:
+                        essid = line.split('ESSID:"')[1].split('"')[0]
+                    except IndexError:
+                        essid = None
+                    result.append(Interface(name, IEEEtype, essid))
+        return result
 
 class AP():
-    def __init__(self, name):
+    def __init__(self, name, protocol, quality, signal):
         self.name = name
+        self.protocol = protocol
+        self.quality = quality
+        self.signal = signal
 
     def __repr__(self):
-        return self.name
+        return "ESSID: %s PROTO: %s SIG: %i%%" % (self.name, self.protocol, self.signal)
 
-def findIF():
-    out = subprocess.check_output(["iwconfig"], stderr = open("/dev/null"))
-    out = out.decode('utf-8')
-    result = []
-    interfaces = out.split('\n\n')
-    interfaces = [x for x in interfaces if x]
-    for interface in interfaces:
-        for line in interface.split("\n"):
-            if not line.startswith(" "):
-                name = line.split()[0]
-                try:
-                    IEEEtype = line.split("IEEE ")[1].split()[0]
-                except IndexError:
-                    IEEEtype = None
-                try:
-                    essid = line.split('ESSID:"')[1].split('"')[0]
-                except IndexError:
-                    essid = None
-                result.append(Interface(name, IEEEtype, essid))
-    return result
+class Network():
+    def __init__(self, ssid, psk):
+        self.ssid = ssid
+        self.psk = psk
+    
+    def __repr__(self):
+        return "ssid: %s" % (self.ssid)
 
 if __name__ == "__main__":
-    interfaces = findIF()
+    interfaces = Interface.findIF()
     for interface in interfaces:
-        print("found interface", interface)
-        #interface.loadCFG()
-        if not interface.state:
-            interface.setUp()
+        print("found interface %s, connected to %s" % (interface, interface.essid))
+        interface.loadCFG()
+        print("wpa_supplicant configured networks")
+        for network in interface.networks:
+            print("\t", network)
+        #if not interface.state:
+        #    interface.setUp()
         aps = interface.scan()
-        print(aps)
+        print("scanned access points")
+        for ap in aps:
+            print("\t", ap)
